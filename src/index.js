4372259b60917d1e9f947fc4b3d5a86ff93e7d68
Andres r hernandez <frodriguezh@unal.edu.co>
2021-10-05 07:12:00 -0500

    creando archivos iniciales y configuración inicial de exrpess

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+// Routes
+
+//Static files
+
+
+//Starting the server
+
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`)
+});
