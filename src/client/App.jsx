94d4f9ae898bf381fa0829943212e208f9211359
Andres r hernandez <frodriguezh@unal.edu.co>
2021-10-06 20:52:05 -0500

    programación y peticiones frontend

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,26 +6,49 @@ const App = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [id, setId] = useState('');
 
 
   const addTask = (e) => {
-    fetch('/api/tasks', {
-      method: 'POST',
-      body: JSON.stringify({ title, description }),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-      .then(data => {
-        console.log(data)
-        M.toast({ html: 'Task saved' })
-        setTitle('')
-        setDescription('')
-
+    if (id != "") {
+      fetch(`/api/tasks/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          id, title, description
+        }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
       })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          M.toast({ html: "Task Updated" })
+          setTitle("")
+          setDescription("")
+          setId("")
+          fetchTasks()
+        })
+    } else {
+      fetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+        .then(data => {
+          console.log(data)
+          M.toast({ html: 'Task saved' })
+          setTitle('')
+          setDescription('')
+          fetchTasks()
+        })
 
-      .catch(err => console.error(err))
+        .catch(err => console.error(err))
+    }
     e.preventDefault();
   }
 
@@ -39,11 +62,38 @@ const App = () => {
     fetch('/api/tasks')
       .then(res => res.json())
       .then(data => {
-        setTasks([...tasks, data])
-        console.log(tasks)
+        setTasks([...data])
+        //console.log(tasks)
       })
   }
 
+  const deleteTasks = (id) => {
+    if (confirm("Are you sure you want to delete it?")) {
+      fetch(`/api/tasks/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          M.toast({ html: 'Task Deleted' })
+          fetchTasks()
+        })
+    }
+  }
+
+  const editTasks = (id) => {
+    fetch(`api/tasks/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setTitle(data.title)
+        setDescription(data.description)
+        setId(data._id)
+      })
+  }
 
 
   const handleChange = (e) => {
@@ -102,16 +152,28 @@ const App = () => {
                 </tr>
               </thead>
               <tbody>
+
                 {
-                  tasks.map(task => {
-                    return (
-                      <tr>
-                        <td>{task.title}</td>
-                        <td>{task.description}</td>
-                      </tr>
-                    )
-                  })
-                }
+                  tasks.length > 0 ? (
+                    tasks.map(task => {
+                      return (
+                        <tr key={task._id}>
+                          <td>{task.title}</td>
+                          <td>{task.description}</td>
+                          <td>
+                            <button className="btn light-blue darken-4" onClick={() => deleteTasks(task._id)}>
+                              <i className="material-icons">delete</i>
+                            </button>
+                            <button className="btn light-blue darken-4" style={{ margin: "4px" }} onClick={() => { editTasks(task._id) }}>
+                              <i className="material-icons">
+                                edit
+                              </i>
+                            </button>
+                          </td>
+                        </tr>
+                      )
+                    })
+                  ) : <p>none</p>}
               </tbody>
             </table>
           </div>
